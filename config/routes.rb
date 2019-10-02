1ad7cc2f3773c716b7cb015b6b36e4281b4cfe13
Heo <tlinhhpu2@gmail.com>
2019-10-02 08:57:28 +0700

    Giao diện sign up, login and Sign up, login
    Giao diện sigup, login and Sigup, login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "sessions/new"
+  root "sessions#new"
+  post "/signup",  to: "users#create"
+  get  "/signup",  to: "users#new"
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+  resources :users
+  resources :characters
+  resources :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,3 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2019_09_25_154111) do
 
   create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -65,8 +53,8 @@ ActiveRecord::Schema.define(version: 2019_09_25_154111) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email"
-    t.string "password"
-    t.string "remember"
+    t.string "password_digest"
+    t.string "remember_digest"
     t.string "activation"
     t.string "full_name"
     t.datetime "dob"
